b5ae35867a665fba31c93e15e0a191f8b89e15ea
Predrag <predrag.babic@tactix-sports.com>
2024-07-03 09:12:14 +0200

    made some changes

--- form.js
+++ form.js
@@ -36,16 +36,70 @@ form.addEventListener("submit", (event) => {
   tagElement.classList.add("card__tag-list-item");
   //console.log(tagElement);
 
-  tagList.append(tagElement);
+  //Create bookmark form card
 
+  const bookmarkCard = document.createElement("div");
+  bookmarkCard.classList.add("card__button-bookmark");
+  console.log(bookmarkCard);
+
+  //Create bookmark form button
+
+  /*const bookmarkFormButton = document.createElement("button");
+  bookmarkFormButton.classList.add("bookmark");
+  bookmarkFormButton.setAttribute("aria-label", "bookmark");
+  bookmarkFormButton.setAttribute("type", "button");
+  // console.log(bookmarkFormButton);
+
+  //SVG Element create
+
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.classList.add("bookmark__icon");
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svg.setAttribute("viewBox", "0 0 24 24");
+
+  //Path create
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute(
+    "d",
+    "M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z"
+  );
+
+  tagList.append(tagElement);
+  svg.append(path);
+  bookmarkFormButton.append(svg);
+  bookmarkCard.append(bookmarkFormButton);*/
+
+  bookmarkCard.innerHTML = `
+             <button
+                class="bookmark"
+                aria-label="bookmark"
+                type="button"
+                data-js="bookmark-button3"
+              >
+                <svg
+                  class="bookmark__icon"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewbox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path
+                    d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z"
+                  />
+                </svg>
+              </button>
+          
+`;
   // Append elements to card
   card.append(questionElement);
   card.append(answerElement);
   card.append(tagList);
   card.append(buttonElement);
+  card.append(bookmarkCard);
 
   // Append card to card container
   cardContainer.append(card);
+  console.log(cardContainer);
 
   buttonElement.addEventListener("click", () => {
     answerElement.classList.toggle("card__answer--active");
